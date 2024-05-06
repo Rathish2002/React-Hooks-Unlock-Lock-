8ec3fa76157e8734c82527264a7c43609fc69c14
Rathish2002 <160592269+Rathish2002@users.noreply.github.com>
2024-05-06 17:16:40 +0530

    styledComponents.js

---
+++ Unlock-Lock Device/styledComponents.js
@@ -0,0 +1,33 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+    background-color: #161617;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Image = styled.img`
+    height: 170px;
+    width: 170px;
+`
+
+export const Paragraph = styled.p`
+    color: #e2e8f0;
+    font-size: 30px;
+    font-family: 'Roboto';
+`
+
+export const Button = styled.button`
+    background-color: #06b6d4;
+    color: #ffffff;
+    font-family: 'Roboto';
+    border-radius: 17px;
+    border-width: 0px;
+    height: 60px;
+    width: 150px;
+    margin-top: 100px;
+`
